3a132fe4100fe4ff6ff8cfa73ebc7a41d7ba0265
Manish Soni <manish.soni@dynatechconsultancy.com>
2026-06-23 08:25:41 +0300

    Before Production Changes on ther Server

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 3000,
+    allowedHosts:["be-aramco-01.bahra-cables.com"],
     proxy: {
       '/api': {
         target: 'http://localhost:8000',
